60a80b377fd77bf7042b19f4c52c0f8bbbf053ab
Aymen <aymen.chahed.13@gmail.com>
2025-12-02 14:10:29 +0100

    add Stream route and link to header navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ColorChangerComponent } from './color-changer/color-changer.component';
 import { CvComponent } from './cv/cv.component';
 import { DetailCvComponent } from './detail-cv/detail-cv.component';
 import { HomeComponent } from './home/home.component';
+import { StreamComponent } from './stream/stream.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'color', component: ColorChangerComponent },
   { path: 'cv', component: CvComponent },
+  { path: 'stream', component: StreamComponent },
   { path: 'cv/:id', component: DetailCvComponent }
 ];
